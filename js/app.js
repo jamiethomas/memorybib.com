bbf83af38a2343f4909cf96aa10f0e1b1dd1518b
Jamie Thomas <james.b.thomas@gmail.com>
2025-02-18 19:01:22 -0500

    Updating site with AlpineJS version. Prior to removing legacy code

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
     // create the module and name it app
         // also include ngRoute for all our routing needs
-    var app = angular.module('app', ['ngRoute']);
+    var app = angular.module('app', ['ngRoute', 'firebase']);
 
     // configure our routes
     app.config(function($routeProvider) {
@@ -34,9 +34,29 @@
     });
 
     // create the controller and inject Angular's $scope
-    app.controller('mainController', function($scope) {
+    app.controller('mainController', function($scope, $firebaseArray) {
         // create a message to display in our view
         $scope.message = 'Everyone come and see how good I look!';
+        $scope.name = "";
+
+        var ref = new Firebase("https://vivid-inferno-2726.firebaseio.com");
+
+        // create a synchronized array
+        // click on `index.html` above to see it used in the DOM!
+        $scope.names = $firebaseArray(ref);
+
+        // add new items to the array
+        // the message is automatically added to our Firebase database!
+        $scope.addMessage = function() {
+          $scope.names.$add({
+            text: $scope.name
+          });
+        };
+
+        $scope.removeMessage = function(name) {
+          $scope.names.$remove(name);
+        };
+
     });
 
     app.controller('aboutController', function($scope) {
