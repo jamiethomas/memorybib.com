b7832a8cac1c27491e3ce2f16cbe26695f6169f2
Jamie Thomas <james.b.thomas@gmail.com>
2025-02-24 19:36:33 -0500

    Add index.js and add functionality

---
+++ index.js
@@ -0,0 +1,52 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('bibForm', () => ({
+        names: [''],
+
+        placeHolderText(index) {
+            let lineNumber = index + 1;
+            return `Line ${lineNumber} text...`;
+        },
+
+        validateForm() {
+            this.errors = {};
+
+            if (this.username.length < 3) {
+                this.errors.username = 'Username must be at least 3 characters';
+            }
+
+            if (this.password.length < 6) {
+                this.errors.password = 'Password must be at least 6 characters';
+            }
+
+            if (this.password !== this.passwordConfirm) {
+                this.errors.passwordConfirm = 'Passwords do not match';
+            }
+
+            if (!this.belt) {
+                this.errors.belt = 'Please select a belt color';
+            }
+
+            if (this.bio.length < 10) {
+                this.errors.bio = 'Bio must be at least 10 characters';
+            }
+        },
+
+        submitForm($event) {
+            this.validateForm();
+            console.log(this.errors);
+
+            if (Object.keys(this.errors).length === 0) {
+                console.log(
+                    this.username,
+                    this.password,
+                    this.passwordConfirm,
+                    this.belt,
+                    this.bio,
+                    this.newsletter
+                );
+
+                $event.target.reset();
+            }
+        },
+    }));
+});
